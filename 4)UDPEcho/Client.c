164746721be3bb34935b06c438e127387ab2db68
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 22:52:08 +0530

    Update Client.c

--- 4)UDPEcho/Client.c
+++ 4)UDPEcho/Client.c
@@ -7,42 +7,38 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-#define PORT 5000
-#define MAXLINE 1000
+#define MAXLINE 1024
+#define PORT 5035
 
-// Driver code
-int main()
-{
-	char buffer[100];
-	char *message = "Hello Server";
-	int sockfd, n;
-	struct sockaddr_in servaddr;
-	
-	// clear servaddr
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	servaddr.sin_port = htons(PORT);
-	servaddr.sin_family = AF_INET;
-	
-	// create datagram socket
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	
-	// connect to server
-	if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-	{
-		printf("\n Error : Connect Failed \n");
-		exit(0);
-	}
+int main(){
+  // socket descriptor creation in udp mode
+  int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+  
+  // for storing  address of address
+  socklen_t addressLength;
+  
+  // preparing message 
+  char sendMessage[MAXLINE],recvMessage[MAXLINE];
+  printf("\nEnter message :");
+  fgets(sendMessage,sizeof(sendMessage),stdin);
+  
+  // storing address in serverAddress
+  struct sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serverAddress.sin_port = htons(PORT);
+  
+  // storing address size 
+  addressLength = sizeof(serverAddress);
 
-	// request to send datagram
-	// no need to specify server address in sendto
-	// connect stores the peers IP and port
-	sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)NULL, sizeof(servaddr));
-	
-	// waiting for response
-	recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL);
-	puts(buffer);
+  // checking connection
+  connect(serverDescriptor,(struct sockaddr*)&serverAddress,addressLength);
+  
+  // sending and receiving the messages 
+  sendto(serverDescriptor,sendMessage,MAXLINE,0,(struct sockaddr*)&serverAddress,addressLength);
+  recvfrom(serverDescriptor,recvMessage,MAXLINE,0,NULL,NULL);
+ 
+  printf("\nServer's Echo : %s\n",recvMessage);
 
-	// close the descriptor
-	close(sockfd);
+  return 0;
 }
