6885da842d0d3eb285b66d9611572e12aa5176e9
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 20:46:22 +0530

    Add files via upload

---
+++ 5)Date_Time/Client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <unistd.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+
+int main() {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation failed");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connection failed");
+        return -1;
+    }
+
+    // Receive date and time from server
+    valread = read(sock, buffer, sizeof(buffer));
+    printf("Received: %s\n", buffer);
+
+    return 0;
+}
