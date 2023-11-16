d85dd6097d10c00281a0625f96c3e52018a759f5
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 22:56:08 +0530

    Update Server.c

--- 7)Full_Duplex/Server.c
+++ 7)Full_Duplex/Server.c
@@ -1,72 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
 
-int main() {
-    int serverSocket, newSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrSize = sizeof(clientAddr);
-    char buffer[1024];
+int main(int argc,char *argv[])
+{
+int clientSocketDescriptor,socketDescriptor;
 
-    // Create socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+struct sockaddr_in serverAddress,clientAddress;
+socklen_t clientLength;
 
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(12345); // Choose a port
+char recvBuffer[1000],sendBuffer[1000];
+pid_t cpid;
+bzero(&serverAddress,sizeof(serverAddress));
+/*Socket address structure*/
+serverAddress.sin_family=AF_INET;
+serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+serverAddress.sin_port=htons(5500);
+/*TCP socket is created, an Internet socket address structure is filled with
+wildcard address & server’s well known port*/
+socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
+/*Bind function assigns a local protocol address to the socket*/
+bind(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
+/*Listen function specifies the maximum number of connections that kernel should queue
+for this socket*/
+listen(socketDescriptor,5);
+printf("%s\n","Server is running ...");
+/*The server to return the next completed connection from the front of the
+completed connection Queue calls it*/
+clientSocketDescriptor=accept(socketDescriptor,(struct sockaddr*)&clientAddress,&clientLength);
+/*Fork system call is used to create a new process*/
+cpid=fork();
 
-    // Bind
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
+if(cpid==0)
+{
+while(1)
+{
+bzero(&recvBuffer,sizeof(recvBuffer));
+/*Receiving the request from client*/
+recv(clientSocketDescriptor,recvBuffer,sizeof(recvBuffer),0);
+printf("\nCLIENT : %s\n",recvBuffer);
+}
+}
+else
+{
+while(1)
+{
 
-    // Listen
-    if (listen(serverSocket, 5) == -1) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server is listening...\n");
-
-    // Accept connection
-    newSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrSize);
-    if (newSocket == -1) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Connection established with client\n");
-
-    while (1) {
-        // Receive message from client
-        ssize_t bytesRead = recv(newSocket, buffer, sizeof(buffer), 0);
-        if (bytesRead == -1) {
-            perror("Receive failed");
-            exit(EXIT_FAILURE);
-        } else if (bytesRead == 0) {
-            printf("Client disconnected\n");
-            break;
-        }
-
-        printf("Received from client: %.*s\n", (int)bytesRead, buffer);
-
-        // Echo the message back to the client
-        if (send(newSocket, buffer, bytesRead, 0) == -1) {
-            perror("Send failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    close(newSocket);
-    close(serverSocket);
-
-    return 0;
+bzero(&sendBuffer,sizeof(sendBuffer));
+printf("\nType a message here ...  ");
+/*Read the message from client*/
+fgets(sendBuffer,10000,stdin);
+/*Sends the message to client*/
+send(clientSocketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
+printf("\nMessage sent !\n");
+}
+}
+return 0;
 }
