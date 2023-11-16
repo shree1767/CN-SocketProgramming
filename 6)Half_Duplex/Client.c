383f55dad54819a6ad3d5493a0717bc2260899ec
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 22:54:10 +0530

    Update Client.c

--- 6)Half_Duplex/Client.c
+++ 6)Half_Duplex/Client.c
@@ -1,44 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+//headers for socket and related functions
+#include <sys/types.h>
+#include <sys/socket.h>
+//for including structures which will store information needed
+#include <netinet/in.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+//for gethostbyname
+#include "netdb.h"
+#include "arpa/inet.h"
 
-int main() {
-    int clientSocket;
-    struct sockaddr_in serverAddr;
-    char buffer[1024];
+//defines
+#define h_addr h_addr_list[0] /* for backward compatibility */
 
-    // Create socket
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+#define PORT 9002 // port number
+#define MAX 1000  //maximum buffer size
+
+//main function
+int main(){
+    char serverResponse[MAX];
+    char clientResponse[MAX];
+
+    //creating a socket
+    int socketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+
+    //placeholder for the hostname and my ip address
+    char hostname[MAX], ipaddress[MAX];
+struct hostent *hostIP; //placeholder for the ip address
+//if the gethostname returns a name then the program will get the ip address
+if(gethostname(hostname,sizeof(hostname))==0){
+    hostIP = gethostbyname(hostname);//the netdb.h fucntion gethostbyname
+}else{
+printf("ERROR:FCC4539 IP Address Not ");
+}
+
+struct sockaddr_in serverAddress;
+serverAddress.sin_family = AF_INET;
+serverAddress.sin_port = htons(PORT);
+serverAddress.sin_addr.s_addr = INADDR_ANY; 
+
+connect(socketDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+// getting the address port and remote host
+    printf("\nLocalhost: %s\n", inet_ntoa((struct in_addr)hostIP->h_addr));
+    printf("Local Port: %d\n", PORT);
+    printf("Remote Host: %s\n", inet_ntoa(serverAddress.sin_addr));
+
+    while (1)
+    {   //recieve the data from the server
+        recv(socketDescriptor, serverResponse, sizeof(serverResponse), 0);
+            //recieved data from the server successfully then printing the data obtained from the server
+            printf("\nSERVER : %s", serverResponse);
+        
+    printf("\ntext message here... :");
+    scanf("%s", clientResponse);
+    send(socketDescriptor, clientResponse, sizeof(clientResponse), 0);
     }
 
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(12345); // Same port as in the server
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP address
-
-    // Connect to server
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
-        perror("Connect failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Connected to server\n");
-
-    // Send message to server
-    char message[] = "Hello, server!";
-    if (send(clientSocket, message, sizeof(message), 0) == -1) {
-        perror("Send failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Sent to server: %s\n", message);
-
-    close(clientSocket);
-
+    //closing the socket
+    close(socketDescriptor);
     return 0;
 }
