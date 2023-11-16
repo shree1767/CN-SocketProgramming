ed5dc0779140497e310ac00f411c2aa688bfb5c6
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 23:15:16 +0530

    Update Client.c

--- 8)FTP/Client.c
+++ 8)FTP/Client.c
@@ -1,56 +1,42 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+//headers for socket and related functions
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+//for including structures which will store information needed
 #include <netinet/in.h>
-#include <arpa/inet.h> // Added for inet_addr
-#include <netdb.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+//for gethostbyname
+#include "netdb.h"
+#include "arpa/inet.h"
 
-#define SERV_TCP_PORT 5035
-#define MAX 60
+// defining constants
+#define PORT 9002
 
-int main(int argc, char *argv[]) {
-    int sockfd, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char send[MAX], recvline[MAX], s[MAX], name[MAX];
+int main()
+{
+  
+    int serverDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddress;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+    char buffer[100], file[1000];
+    
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(PORT);
+
+    connect(serverDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
+    
+    while (1){
+        printf("File name : ");
+        scanf("%s",buffer);
+        send(serverDescriptor,buffer,strlen(buffer)+1,0);
+        printf("%s\n","File Output : ");
+        recv(serverDescriptor,&file,sizeof(file),0);
+        printf("%s",file);
     }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(SERV_TCP_PORT);
-
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
-        perror("Connection failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("\nEnter the source file name : \n");
-    if (scanf("%s", send) != 1) {
-        perror("Error reading input");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (write(sockfd, send, MAX) == -1) {
-        perror("Write failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    while ((n = read(sockfd, recvline, MAX)) != 0) {
-        printf("%s", recvline);
-    }
-
-    close(sockfd);
-
     return 0;
-}
+}    
