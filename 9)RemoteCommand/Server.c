8444035711e2113c4913f9a19b852a8b89805db7
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 23:17:03 +0530

    Update Server.c

--- 9)RemoteCommand/Server.c
+++ 9)RemoteCommand/Server.c
@@ -1,62 +1,38 @@
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/stat.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#define MAX 1000
+int main()
+{
 
-#define PORT 8888
-#define BUFFER_SIZE 1024
+    int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    int size;
+    char buffer[MAX], message[] = "Command Successfully executed !";
+    struct sockaddr_in clientAddress, serverAddress;
 
-int main() {
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE];
+    socklen_t clientLength = sizeof(clientAddress);
 
-    // Create a UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Error in socket creation");
-        exit(1);
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(9976);
+
+    bind(serverDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    while (1)
+    {
+        bzero(buffer, sizeof(buffer));
+        recvfrom(serverDescriptor, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddress, &clientLength);
+        system(buffer);
+        printf("Command Executed ... %s ", buffer);
+        sendto(serverDescriptor, message, sizeof(message), 0, (struct sockaddr *)&clientAddress, clientLength);
     }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Bind the socket to the server address
-    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Error in binding");
-        exit(1);
-    }
-
-    printf("Server is listening on port %d...\n", PORT);
-
-    while (1) {
-        // Receive a command from the client
-        memset(buffer, 0, sizeof(buffer));
-        recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &client_addr_len);
-        printf("Received command: %s\n", buffer);
-
-        // Execute the command and capture the output
-        FILE* cmd_output = popen(buffer, "r");
-        if (cmd_output == NULL) {
-            perror("Error executing command");
-            continue;
-        }
-
-        // Send the output back to the client
-        memset(buffer, 0, sizeof(buffer));
-        while (fgets(buffer, sizeof(buffer), cmd_output) != NULL) {
-            sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, client_addr_len);
-            memset(buffer, 0, sizeof(buffer));
-        }
-
-        // Close the command output
-        pclose(cmd_output);
-    }
-
-    close(sockfd);
+    close(serverDescriptor);
     return 0;
 }
