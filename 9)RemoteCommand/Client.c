767c75dcf5ff3db92994a65829794cf62d20e5a0
Shreenidhi Sashisekar <93395263+shree1767@users.noreply.github.com>
2023-11-16 23:16:33 +0530

    Update Client.c

--- 9)RemoteCommand/Client.c
+++ 9)RemoteCommand/Client.c
@@ -1,46 +1,35 @@
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <arpa/inet.h>
+#define MAX 1000
 
-#define SERVER_IP "127.0.0.1"
-#define PORT 8888
-#define BUFFER_SIZE 1024
+int main()
+{
+    int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    char buffer[MAX], message[MAX];
+    struct sockaddr_in cliaddr, serverAddress;
+    socklen_t serverLength = sizeof(serverAddress);
 
-int main() {
-    int sockfd;
-    struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
+    bzero(&serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(9976);
 
-    // Create a UDP socket
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        perror("Error in socket creation");
-        exit(1);
+    bind(serverDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+    while (1)
+    {
+        printf("\nCOMMAND FOR EXECUTION ... ");
+        fgets(buffer, sizeof(buffer), stdin);
+        sendto(serverDescriptor, buffer, sizeof(buffer), 0, (struct sockaddr *)&serverAddress, serverLength);
+        printf("\nData Sent !");
+        recvfrom(serverDescriptor, message, sizeof(message), 0, (struct sockaddr *)&serverAddress, &serverLength);
+        printf("UDP SERVER : %s", message);
     }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-    while (1) {
-        printf("Enter a command to execute on the server: ");
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-            perror("Error reading input");
-            break; // Exit the loop on error
-        }
-
-        // Send the command to the server
-        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-        // Receive and display the output from the server
-        memset(buffer, 0, sizeof(buffer));
-        recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
-        printf("Server response:\n%s\n", buffer);
-    }
-
-    close(sockfd);
     return 0;
 }
